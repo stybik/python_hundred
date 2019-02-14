804eb906d022bef9495c3c626b59b860ea345417
stybik <bikrocksbiswas@gmail.com>
2019-02-14 18:00:56 +0530

    one solved

--- ex_by_rane/ex4.py
+++ ex_by_rane/ex4.py
@@ -1,1 +1,6 @@
 #Make last word of each word in sentence capital
+
+sentence = raw_input("Enter a sentence : ")
+new_sentence = (" ".join(word[::-1] for word in sentence.split(" "))).title()
+new_sentence = " ".join(word[::-1] for word in new_sentence.split(" "))
+print new_sentence
